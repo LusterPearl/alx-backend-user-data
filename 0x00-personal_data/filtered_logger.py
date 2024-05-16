74a0bbcb6e7ac9c093879f879a3f78de0cd2cbf4
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-16 12:57:00 +0100

    filtert0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,18 +7,18 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+                 fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Returns the log message obfuscated.
-    
     Arguments:
     - fields: List of strings representing all fields to obfuscate.
     - redaction: String representing what the field will be obfuscated with.
     - message: String representing the log line.
     - separator: String representing by which character the fields.
-    
     Returns:
     - Obfuscated log message.
     """
     pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
-    return re.sub(pattern, lambda m: f'{m.group().split("=")[0]}={redaction}', message)
+    return re.sub(pattern, lambda m: f'{
+                  m.group().split("=")[0]}={redaction}', message)
