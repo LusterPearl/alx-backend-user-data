c45a728e71b074995e26347cfae613c21ea14597
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-24 12:06:27 +0100

    go

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,6 +29,7 @@ class BasicAuth(Auth):
         except:
             return None
         
+    
     def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
         """Extract user email and password from decoded Base64 authorization header."""
         if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
@@ -39,4 +40,23 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(":", 1)
         return email, password
+    
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Retrieve User instance based on email and password."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
 
