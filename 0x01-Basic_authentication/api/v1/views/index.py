c3ac1e95cdfbe42b5d3527675fe26dd4be5a641e
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-24 09:43:45 +0100

    hope

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
-"""this script contains the api endpoints"""
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-from flask import Blueprint, abort
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
 
 
-app_views = Blueprint('app_views', __name__)
-
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
-def unauthorized_route():
-    """define a route that always returns a 401"""
-    abort(401)
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
