c60c7da50bf59906d3e72ca561b375e010a56605
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-25 19:44:55 +0100

    hh

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,23 +4,36 @@
 
 import uuid
 from api.v1.auth.auth import Auth
+from api.v1.views.session_auth import *
 
 
 class SessionAuth(Auth):
-    """Session authentication class"""
+    """Session Authentication"""
+    
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Create a session ID for a user_id"""
+        """Creates a Session ID for a user ID"""
         if user_id is None or not isinstance(user_id, str):
             return None
-
-        session_id = str(uuid.uuid4())
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Returns a User ID based on a Session ID"""
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns a User instance based on a cookie value"""
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
