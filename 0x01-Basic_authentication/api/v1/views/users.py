380257740ac6929e8f2b7f3997671e80e5de95ee
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-27 10:54:40 +0100

    user2

--- 0x01-Basic_authentication/api/v1/views/users.py
+++ 0x01-Basic_authentication/api/v1/views/users.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
+from models import storage
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
