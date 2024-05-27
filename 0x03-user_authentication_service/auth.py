27870efb6ba9a72bb97f39aa4a385fb89912028b
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-27 15:34:30 +0100

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,17 +3,49 @@
 Auth module
 """
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
-def _hash_password(password: str) -> bytes:
-    """Hashes a password with bcrypt and returns the hashed.
 
-    Args:
-        password (str): The password to hash.
-
-    Returns:
-        bytes: The hashed password.
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode(), salt)
-    return hashed
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes a password with bcrypt and returns the hashed password as bytes.
+
+        Args:
+            password (str): The password to hash.
+
+        Returns:
+            bytes: The hashed password.
+        """
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode(), salt)
+        return hashed
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user with the given email and password.
+
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
+
+        Returns:
+            User: The newly created user.
+
+        Raises:
+            ValueError: If the user already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
