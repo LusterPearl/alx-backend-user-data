cbca0a8c0624a05e943b16e77cd947a5460b562b
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-28 17:01:02 +0100

    auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -109,5 +109,17 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        auth.update_password(email, reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 403
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
