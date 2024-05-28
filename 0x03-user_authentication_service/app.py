82ce0853ed4f49b158e70edd78ad0cd6574a3bf2
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-28 13:36:37 +0100

    hashpasswd

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,10 +3,13 @@
 Basic Flask app
 """
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, abort
 from auth import Auth
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from werkzeug.security import check_password_hash
+import uuid
+
 
 app = Flask(__name__)
 
@@ -33,6 +36,32 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+    
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Endpoint to log in a user and create a new session."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        abort(401)
+
+    try:
+        user = AUTH.get_user(email)
+    except NoResultFound:
+        abort(401)
+
+    if not check_password_hash(user.hashed_password, password):
+        abort(401)
+
+    session_id = str(uuid.uuid4())
+    user.session_id = session_id
+    AUTH.update_user(user.id, session_id=session_id)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
