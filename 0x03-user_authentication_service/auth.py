8cc2d7c6f05d0e7c1fbf317b9f92dbcad0050189
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-28 15:48:08 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -134,8 +134,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError(f"User with email {email} not found")
-        
+
         reset_token = str(uuid.uuid4())
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
-
