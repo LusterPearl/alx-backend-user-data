2de1cf14beae1ae37600534c81b88fb9d63dfb0c
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-30 11:59:45 +0100

    original

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -139,13 +139,18 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
-    def update_password(self, reset_token: str, new_password: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user's password using reset_token."""
         try:
-            user = self._db.find_user_by(reset_token=reset_token)
-            user.password = new_password
-            self._db.save(user)
+            user = self._db.find_user_by(
+                   reset_token=reset_token)
         except NoResultFound:
             raise ValueError("Invalid reset token")
+
+        hashed_password = self._hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
 
     def _hash_password(self, password: str) -> bytes:
         """Hashes a password with bcrypt and returns as bytes."""
